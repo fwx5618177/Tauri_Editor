3df54d1be59bfc91c4fae3fdc02acc3d56d1e825
moxi.feng <moxi.feng@advantest.com>
2022-09-08 18:33:48 +0800

    feat(component): logic flow

--- src/views/logicflow/LogicFlow.tsx
+++ src/views/logicflow/LogicFlow.tsx
@@ -1,34 +1,78 @@
 import { Graph } from '@antv/x6'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const LogicFlow = () => {
     const graphRef = useRef<HTMLDivElement>(null)
+    const [graphObj, setGraphObj] = useState<Graph>(null as unknown as Graph)
 
-    useEffect(() => {
-        console.log(graphRef.current)
+    const initial = () => {
+        ;(graphRef.current as HTMLDivElement).style.width = document.body.clientWidth + 'px'
+        ;(graphRef.current as HTMLDivElement).style.height = document.body.clientHeight + 'px'
+
+        window.addEventListener('resize', () => {
+            ;(graphRef.current as HTMLDivElement).style.width = document.body.clientWidth + 'px'
+            ;(graphRef.current as HTMLDivElement).style.height = document.body.clientHeight + 'px'
+        })
+
         const graph = new Graph({
             container: graphRef.current as HTMLDivElement,
             grid: true,
             autoResize: true,
         })
 
-        graph.addNode({
-            x: 160,
-            y: 220,
-            width: 360,
-            // height: 520,
-            shape: 'text-block',
+        setGraphObj(graph)
+    }
+
+    interface NodeInfo {
+        x?: number
+        y?: number
+        width?: number
+        height?: number
+        text: string
+    }
+
+    const nodes: NodeInfo[] = [
+        {
             text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.`,
-            attrs: {
-                body: {
-                    fill: '#efdbff',
-                    stroke: '#9254de',
-                    rx: 4,
-                    ry: 4,
-                },
+        },
+        {
+            text: `fwx`,
+        },
+    ]
+
+    const conf = nodes.map((ci, index) => ({
+        x: 160 + index * 20,
+        y: 220 + index * 20,
+        // width: 360,
+        // height: 120,
+        width: ci.text.length * 40 > 300 ? 300 : ci.text.length * 40,
+        height: (ci.text.length * 40 - 300) / 50 > 100 ? (ci.text.length * 40 - 300) / 50 : 100,
+        shape: 'text-block',
+        text: ci.text,
+        attrs: {
+            body: {
+                fill: '#efdbff',
+                stroke: '#9254de',
+                rx: 4,
+                ry: 4,
             },
-        })
+        },
+    }))
+
+    const graphNode = () => {
+        graphObj.addNodes(conf)
+    }
+
+    useEffect(() => {
+        initial()
     }, [])
+
+    useEffect(() => {
+        if (graphObj) {
+            graphObj.clearBackground()
+            graphNode()
+        }
+    }, [graphObj])
 
     return (
         <>
@@ -43,7 +87,6 @@ const LogicFlow = () => {
                 id='container'
                 style={{
                     display: 'flex',
-                    minHeight: 800,
                 }}
                 ref={graphRef}
             ></div>
